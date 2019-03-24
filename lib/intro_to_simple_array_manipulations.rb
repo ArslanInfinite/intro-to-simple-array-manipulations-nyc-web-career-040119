2e25fa4d5e7d72b03786d67ad980ef1bacf4280b
Arslan Ali <ali.arslan319@gmail.com>
2019-03-24 21:41:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,9 +3,9 @@ def using_push(countries_in_western_africa, next_country)
 end
 
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood )
-  neighborhoods_in_northwest_brooklyn.unshift "Brooklyn Heights"
+  neighborhoods_in_northwest_brooklyn.unshift new_neighborhood
 end
 
 def using_pop(great_hits_of_the_nineties, deleted_string)
-  great_hits_of_the_nineties.pop "Losing My Religion"
+  great_hits_of_the_nineties.pop deleted_string
 end
