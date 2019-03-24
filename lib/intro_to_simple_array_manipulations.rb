1c96f511691b795203fc4b8603ee8343c03f9c9a
Arslan Ali <ali.arslan319@gmail.com>
2019-03-24 23:40:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,11 +33,3 @@ end
 def using_uniq(captain_planet_and_the_planeteers)
   captain_planet_and_the_planeteers.uniq
 end
-
-describe "#using_uniq" do
-  it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
-    captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-    new_array = using_uniq(captain_planet_and_the_planeteers)
-    expect(new_array).to eq(["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler"])
-  end
-end
