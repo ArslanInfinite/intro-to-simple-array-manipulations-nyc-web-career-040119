93e5a8f89e549ee1f0842677368998ff12f20442
Arslan Ali <ali.arslan319@gmail.com>
2019-03-24 21:43:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,5 +7,6 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood )
 end
 
 def using_pop(great_hits_of_the_nineties, deleted_string)
-  great_hits_of_the_nineties.pop deleted_string
+  great_hits_of_the_nineties.pop "Losing My Religion"
 end
+
